cc7abd9057f5737014eb2becc5d31ee07f40894b
yasmenmohamed00 <yasmenmohamedsh@gmail.com>
2019-04-20 23:25:15 +0200

    new message

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -1,9 +1,8 @@
 //Define UI Vars
-let sections = document.querySelector('section');
+let list = document.querySelector('.list');
 let addItem = document.querySelector('#add');
 let noteDate = document.querySelector('.date');
-let newNote = document.querySelector('.note');
-
+let body = document.querySelector('body')
 
 loadEventListener();
 // Load all Event Listeners
@@ -12,12 +11,15 @@ function loadEventListener() {
     addItem.addEventListener('click', addNote);
 
     // Delete this note.
-    sections.addEventListener('click', deleteNote);
+    list.addEventListener('click', deleteNote); 
+
+    body.addEventListener('keydown', writeNote);
+
 }
 
 function addNote(e){
 //create new note
-let newNote = document.createElement('div');
+let newNote = document.createElement('li');
 newNote.className = 'note';
 
  let noteHeader = document.createElement('div');
@@ -29,7 +31,7 @@ newNote.className = 'note';
  noteBody.setAttribute('contenteditable', 'true');
 
  let p = document.createElement('p');
- p.className = 'data';
+ p.className = 'date';
  p.appendChild(document.createTextNode(today));
 
  let noteItem = document.createElement('div');
@@ -39,29 +41,32 @@ newNote.className = 'note';
  newNote.appendChild(noteBody);
  noteHeader.appendChild(p);
  noteHeader.appendChild(noteItem);
- noteItem.innerHTML =  '<i id="note__header-item-check" class="fas fa-pen"></i>' +
- ' <i id="note__header-item-delete" class="far fa-trash-alt"></i>';
-sections.appendChild(newNote);
+ noteItem.innerHTML =  '<i id="note__header-item-check" class="fas fa-check"></i>' +
+                       '<i id="note__header-item-delete" class="far fa-trash-alt"></i>';
+list.insertBefore(newNote, list.childNodes[0]);
+
 
 e.preventDefault();
+}  
+
+
+//write into note
+function writeNote(e){
+    let itemCheck = document.getElementById('note__header-item-check');
+    var timer = null;
+    if(e.type){
+        itemCheck.className = 'fas fa-pen';
+        // console.log('hjfghjkk')
+    }
+    clearTimeout(timer); 
+    timer = setTimeout(stopTyping, 4000);
 }
-// the date when he created the note
-var monthNames = [ "January", "February", "March", "April", "May", "June",
-       "July", "August", "September", "October", "November", "December" ];
-var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];    
-      
-var today = new Date();
-var ddd = today.getDay();
-var mmm = today.getMonth();
-var hour = today.getHours();
-var min = today.getMinutes();
-var ampm = hour >= 12 ? 'PM' : 'AM';
-    hour = hour % 12;
-    hour = hour ? hour : 12; // the hour '0' should be '12'
-    min = min < 10 ? '0'+min : min;
-var yyy = today.getFullYear();
-    today = dayNames[ddd]+' '+0+ddd+' '+monthNames[mmm] +','+' '+yyy +' '+ 'at'+' '+ hour+':'+min+ampm;
-  
+//stop typing in the note and save it
+function stopTyping(){
+    let itemCheck = document.getElementById('note__header-item-check');
+    itemCheck.className = 'fas fa-check';
+}
+
 // Delete this note.
  function deleteNote(e){
    if(e.target.classList.contains('fa-trash-alt')){   
@@ -70,4 +75,35 @@ var yyy = today.getFullYear();
      }
    }
 }
+// the date when he created the note
+var monthNames = [ "January", "February", "March", "April", "May", "June",
+       "July", "August", "September", "October", "November", "December" ];
+var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];    
+      
+var today = new Date();
+    ddd = today.getDay();
+    day = today.getDate()
+    mmm = today.getMonth();
+    hour = today.getHours();
+    min = today.getMinutes();
+    ampm = hour >= 12 ? 'PM' : 'AM';
+    hour = hour % 12;
+    hour = hour ? hour : 12; // the hour '0' should be '12'
+    min = min < 10 ? '0'+min : min;
+    yyy = today.getFullYear();
+    today = dayNames[ddd]+' '+day+' '+monthNames[mmm] +','+' '+yyy +' '+ 'at'+' '+ hour+':'+min+ampm;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
